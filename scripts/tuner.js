646838010d76ed3a587a320356b6dc08603bdcb3
Ujwal N K <ujwal.nk@gmail.com>
2025-02-06 11:48:16 +0530

    Partial Tuner Implementation

---
+++ scripts/tuner.js
@@ -0,0 +1,50 @@
+/**
+* Author: Website (https://codepen.io/Sm1lEE/pen/YzejLN)
+* Date Imported: 10 Mar, 2023
+**/
+
+var needle = document.querySelector('needle');
+var el = document.querySelector('el');
+
+var measureDeg = function() {
+  // matrix-to-degree conversion from https://css-tricks.com/get-value-of-css-rotation-through-javascript/
+  var st = window.getComputedStyle(needle, null);
+  var tr = st.getPropertyValue("-webkit-transform") ||
+           st.getPropertyValue("-moz-transform") ||
+           st.getPropertyValue("-ms-transform") ||
+           st.getPropertyValue("-o-transform") ||
+           st.getPropertyValue("transform") ||
+           "fail...";
+
+  var values = tr.split('(')[1];
+      values = values.split(')')[0];
+      values = values.split(',');
+  var a = values[0];
+  var b = values[1];
+  var c = values[2];
+  var d = values[3];
+
+  var scale = Math.sqrt(a*a + b*b);
+
+  // arc sin, convert from radians to degrees, round
+  var sin = b/scale;
+  var angle = Math.round(Math.atan2(b, a) * (180/Math.PI));
+  
+  el.set('data-value', angle-90);
+};
+
+// var periodicalID = measureDeg.periodical(10);
+
+
+
+/**
+ * Author: Ujwal N K
+ */
+
+/**
+ * Function to show the current note on the Instrument Tuner meter
+ * @param {String} note - Note being Played
+ */
+function tunerSetNote(note){
+
+}
